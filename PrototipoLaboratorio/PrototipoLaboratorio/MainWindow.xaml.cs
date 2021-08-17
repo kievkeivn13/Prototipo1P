ab56f14bf7a2c4d8d935b8b84015b84ae6c4700f
kievkevin13 <kievkevin13@gmail.com>
2021-08-17 12:24:03 -0600

    MDI, BD, Diagramas

---
+++ PrototipoLaboratorio/PrototipoLaboratorio/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿//using PrototipoLaboratorio.Ventanas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrototipoLaboratorio
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+        private void btnRptPaciantes_Click(object sender, RoutedEventArgs e)
+        {
+          //  rptPaciente dashboard = new rptPaciente();
+          //  dashboard.Show();
+        }
+        private void funGestorventas(UserControl control)
+        {
+            this.pnlVentanas.Children.Clear();
+            this.pnlVentanas.Children.Add(control);
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            this.pnlVentanas.Children.Clear();
+        }
+
+        private void btnSignOut_Click(object sender, RoutedEventArgs e)
+        {
+            loginscreen dashboard = new loginscreen();
+            dashboard.Show();
+            this.Close();
+        }
+    }
+
+}
---
+++ PrototipoLaboratorio/PrototipoLaboratorio/loginscreen.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PrototipoLaboratorio
+{
+    /// <summary>
+    /// Lógica de interacción para loginscreen.xaml
+    /// </summary>
+    public partial class loginscreen : Window
+    {
+        Conexion cn = new Conexion();
+        public loginscreen()
+        {
+            InitializeComponent();
+            txtUsuario.Focus();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsuario.Text != "" || txtContraseña.Password != "")
+            {
+                try
+                {
+                    string Query = "select COUNT(*) from CLINICA1.USUARIOS where nombre_usuario='" + this.txtUsuario.Text + "' AND passwd_usuario='" + this.txtContraseña.Password + "';";
+
+                    OdbcCommand consulta = new OdbcCommand(Query, cn.conexion());
+                    consulta.ExecuteNonQuery();
+                   
+                    int count = Convert.ToInt32(consulta.ExecuteScalar());
+                    if (count == 1)
+                    {
+                        String user = txtUsuario.Text;
+                        MessageBox.Show("     Bienvenido "+user+"    ");
+                        MainWindow dashboard = new MainWindow();
+                        dashboard.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario o contraseña incorrecta.");
+                        txtContraseña.Password = "";
+                        txtUsuario.Text = ""; txtUsuario.Focus();                        
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                   MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Falta usuario o contraseña.");
+                txtUsuario.Focus();
+            }
+
+        }
+
+      
+        private void txtContraseña_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            { 
+                e.Handled = true;//elimina el sonido
+                Button_Click(sender, e);//llama al evento click del boton
+            }
+        }
+
+        private void txtUsuario_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                e.Handled = true;//elimina el sonido
+                txtContraseña.Focus();
+            }
+        }
+    }
+}
